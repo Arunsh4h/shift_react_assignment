c039af2fb82fd4114a8f9167e94dc85e01a9c717
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2022-11-28 14:31:52 +0530

    minor fix

---
+++ utils/overlapper.js
@@ -0,0 +1,18 @@
+// will return true if shidt is overlaped
+const checkOverlap = allShifts => {
+  let bookedShifts = [];
+  /*
+        [
+            5.5,
+            8.5
+        ]
+
+        7.5
+    */
+  let availableShifts = [];
+
+  allShifts.forEach(shift => {
+    const startTime = shift.startTime;
+    const endTime = shift.endTime;
+  });
+};
